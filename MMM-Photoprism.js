42bc527d7bb38b2325986b01e6b97456136e7012
HeikoGr <20295490+HeikoGr@users.noreply.github.com>
2025-10-01 18:04:20 +0000

    Add linting and formatting scripts; update devDependencies in package.json

--- MMM-Photoprism.js
+++ MMM-Photoprism.js
@@ -1,140 +1,140 @@
 Module.register("MMM-Photoprism", {
-    updateTimer: null,
-    defaults: {
-        apiUrl: "http://photoprism.local:2342",
-        apiKey: "", //see README for how to obtain (curl is easiest)
-        albumId: "", //you can find it in the URL when you browse to your album
-        updateInterval: 5 * 60 * 1000, // 5 minutes in milliseconds
-        fadeSpeed: 1000, // Fade speed in milliseconds
-        maxWidth: "100%",
-        maxHeight: "100%",
-        cacheRetentionDays: 1 // Number of days to keep cached images
-    },
+  updateTimer: null,
+  defaults: {
+    apiUrl: "http://photoprism.local:2342",
+    apiKey: "", // see README for how to obtain (curl is easiest)
+    albumId: "", // you can find it in the URL when you browse to your album
+    updateInterval: 5 * 60 * 1000, // 5 minutes in milliseconds
+    fadeSpeed: 1000, // Fade speed in milliseconds
+    maxWidth: "100%",
+    maxHeight: "100%",
+    cacheRetentionDays: 1 // Number of days to keep cached images
+  },
 
-    getStyles: function() {
-        return ["MMM-Photoprism.css"];
-    },
+  getStyles () {
+    return ["MMM-Photoprism.css"];
+  },
 
-    start: function() {
-        console.log("[MMM-Photoprism] Starting module");
-        this.currentImage = null;
-        this.loaded = false;
-        this.error = null;
+  start () {
+    console.log("[MMM-Photoprism] Starting module");
+    this.currentImage = null;
+    this.loaded = false;
+    this.error = null;
 
-        // Initial configuration send to node helper is done in resume()
-    },
+    // Initial configuration send to node helper is done in resume()
+  },
 
-    getDom: function() {
-        console.log("[MMM-Photoprism] Creating DOM");
-        const wrapper = document.createElement("div");
-        wrapper.className = "photoprism-container";
+  getDom () {
+    console.log("[MMM-Photoprism] Creating DOM");
+    const wrapper = document.createElement("div");
+    wrapper.className = "photoprism-container";
 
-        if (this.error) {
-            console.log("[MMM-Photoprism] Showing error:", this.error);
-            wrapper.innerHTML = `Error: ${this.error}`;
-            return wrapper;
-        }
-
-        if (!this.loaded) {
-            console.log("[MMM-Photoprism] Module not loaded yet or suspended, showing loading message");
-            wrapper.innerHTML = "Loading...";
-            return wrapper;
-        }
-
-        if (this.currentImage) {
-            console.log("[MMM-Photoprism] Creating image element for:", this.currentImage.path);
-            const img = document.createElement("img");
-            img.src = this.currentImage.path;
-            img.className = "photoprism-image";
-            wrapper.appendChild(img);
-
-            if (this.currentImage.title || this.currentImage.location) {
-                console.log("[MMM-Photoprism] Adding title and location");
-                const infoContainer = document.createElement("div");
-                infoContainer.className = "photoprism-info";
-                
-                if (this.currentImage.title) {
-                    const title = document.createElement("div");
-                    title.className = "photoprism-title";
-                    title.innerHTML = this.currentImage.title;
-                    infoContainer.appendChild(title);
-                }
-                
-                if (this.currentImage.location) {
-                    const location = document.createElement("div");
-                    location.className = "photoprism-location";
-                    location.innerHTML = this.currentImage.location;
-                    infoContainer.appendChild(location);
-                }
-                
-                wrapper.appendChild(infoContainer);
-            }
-        } else {
-            console.log("[MMM-Photoprism] No image available to display");
-            wrapper.innerHTML = "No image available";
-        }
-
-        return wrapper;
-    },
-
-    socketNotificationReceived: function(notification, payload) {
-        console.log(`[MMM-Photoprism] Received socket notification: ${notification}`);
-        if (notification === "IMAGE_READY") {
-            console.log("[MMM-Photoprism] New image ready:", payload);
-            this.currentImage = payload;
-            this.loaded = true;
-            this.error = null;
-            this.updateDom(this.config.fadeSpeed);
-        } else if (notification === "ERROR") {
-            console.log("[MMM-Photoprism] Error received:", payload);
-            this.error = payload;
-            this.loaded = true;
-            this.updateDom();
-        }
-    },
-
-    notificationReceived: function(notification, payload, sender) {
-        // Only process notifications we care about
-        if (notification === "DOM_OBJECTS_CREATED") {
-            console.log("[MMM-Photoprism] DOM objects created, starting update interval");
-            // Start the update interval
-            if (this.updateTimer) {
-                clearInterval(this.updateTimer);
-            }
-            this.updateTimer = setInterval(() => {
-                console.log("[MMM-Photoprism] Interval triggered, requesting new image");
-                this.error = null;
-                this.sendSocketNotification("CONFIG", this.config);
-            }, this.config.updateInterval);
-        }
-    },
-
-    suspend: function() {
-        console.log("[MMM-Photoprism] Module suspended");
-
-        this.currentImage = null;
-        this.loaded = false;
-        this.error = null;
-        this.updateDom(this.config.fadeSpeed);
-
-        if (this.updateTimer) {
-            clearInterval(this.updateTimer);
-            this.updateTimer = null;
-        }
-    },
-
-    resume: function() {
-        console.log("[MMM-Photoprism] Module resumed");
-
-        this.sendSocketNotification("CONFIG", this.config);
-        
-        // Intervall neu starten
-        if (!this.updateTimer) {
-            this.updateTimer = setInterval(() => {
-                console.log("[MMM-Photoprism] Interval triggered, requesting new image");
-                this.error = null;
-                this.sendSocketNotification("CONFIG", this.config);
-            }, this.config.updateInterval);
-        }
+    if (this.error) {
+      console.log("[MMM-Photoprism] Showing error:", this.error);
+      wrapper.innerHTML = `Error: ${this.error}`;
+      return wrapper;
     }
-}); 
+
+    if (!this.loaded) {
+      console.log("[MMM-Photoprism] Module not loaded yet or suspended, showing loading message");
+      wrapper.innerHTML = "Loading...";
+      return wrapper;
+    }
+
+    if (this.currentImage) {
+      console.log("[MMM-Photoprism] Creating image element for:", this.currentImage.path);
+      const img = document.createElement("img");
+      img.src = this.currentImage.path;
+      img.className = "photoprism-image";
+      wrapper.appendChild(img);
+
+      if (this.currentImage.title || this.currentImage.location) {
+        console.log("[MMM-Photoprism] Adding title and location");
+        const infoContainer = document.createElement("div");
+        infoContainer.className = "photoprism-info";
+
+        if (this.currentImage.title) {
+          const title = document.createElement("div");
+          title.className = "photoprism-title";
+          title.innerHTML = this.currentImage.title;
+          infoContainer.appendChild(title);
+        }
+
+        if (this.currentImage.location) {
+          const location = document.createElement("div");
+          location.className = "photoprism-location";
+          location.innerHTML = this.currentImage.location;
+          infoContainer.appendChild(location);
+        }
+
+        wrapper.appendChild(infoContainer);
+      }
+    } else {
+      console.log("[MMM-Photoprism] No image available to display");
+      wrapper.innerHTML = "No image available";
+    }
+
+    return wrapper;
+  },
+
+  socketNotificationReceived (notification, payload) {
+    console.log(`[MMM-Photoprism] Received socket notification: ${notification}`);
+    if (notification === "IMAGE_READY") {
+      console.log("[MMM-Photoprism] New image ready:", payload);
+      this.currentImage = payload;
+      this.loaded = true;
+      this.error = null;
+      this.updateDom(this.config.fadeSpeed);
+    } else if (notification === "ERROR") {
+      console.log("[MMM-Photoprism] Error received:", payload);
+      this.error = payload;
+      this.loaded = true;
+      this.updateDom();
+    }
+  },
+
+  notificationReceived (notification, payload, sender) {
+    // Only process notifications we care about
+    if (notification === "DOM_OBJECTS_CREATED") {
+      console.log("[MMM-Photoprism] DOM objects created, starting update interval");
+      // Start the update interval
+      if (this.updateTimer) {
+        clearInterval(this.updateTimer);
+      }
+      this.updateTimer = setInterval(() => {
+        console.log("[MMM-Photoprism] Interval triggered, requesting new image");
+        this.error = null;
+        this.sendSocketNotification("CONFIG", this.config);
+      }, this.config.updateInterval);
+    }
+  },
+
+  suspend () {
+    console.log("[MMM-Photoprism] Module suspended");
+
+    this.currentImage = null;
+    this.loaded = false;
+    this.error = null;
+    this.updateDom(this.config.fadeSpeed);
+
+    if (this.updateTimer) {
+      clearInterval(this.updateTimer);
+      this.updateTimer = null;
+    }
+  },
+
+  resume () {
+    console.log("[MMM-Photoprism] Module resumed");
+
+    this.sendSocketNotification("CONFIG", this.config);
+
+    // Intervall neu starten
+    if (!this.updateTimer) {
+      this.updateTimer = setInterval(() => {
+        console.log("[MMM-Photoprism] Interval triggered, requesting new image");
+        this.error = null;
+        this.sendSocketNotification("CONFIG", this.config);
+      }, this.config.updateInterval);
+    }
+  }
+});
